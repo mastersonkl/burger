73fed415c07d8ab3b580ded4aa6ab40064649d3e
Kendall Masterson <masterson.kendall@gmail.com>
2020-10-08 16:10:35 -0400

    Added seeds.sql file

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burger_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bunrise Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Classic Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bravo Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Gus Burger', false);
