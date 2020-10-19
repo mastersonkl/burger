a5fbf7623830e5b3a66e3e2fdac5037b54b8596d
Kendall Masterson <masterson.kendall@gmail.com>
2020-10-18 20:07:18 -0400

    Updating the orm.js

---
+++ config/orm.js
@@ -0,0 +1,31 @@
+const connection = require("./connection");
+
+var orm = {
+  all: function (tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput;
+    connection.query(queryString, function (err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+
+  firstInsert: function (tableInput, val1, cb) {
+    connection.query(
+      "INSERT INTO " + tableInput + "(burger_name) VALUES ('" + val1 + "');"
+    ),
+      function (err, result) {
+        if (err) throw err;
+      };
+    cb(result);
+  },
+  firstUpdate: function (tableInput, condition, cb) {
+    var queryString = "Update ?? SET devoured=true WHERE ?";
+    connection.query(queryString, [tableInput, condition], function (
+      err,
+      result
+    ) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+};
