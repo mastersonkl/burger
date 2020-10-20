655ef7068b7d14904696c2ab7388886ba69f2114
Kendall Masterson <masterson.kendall@gmail.com>
2020-10-19 21:18:51 -0400

    Updated burgers_controller.js with routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,20 +1,47 @@
 var express = require("express");
 var router = express.Router();
-var burgers = require("../models/burger");
+var burger = require("../models/burger.js");
 
 router.get("/", function (req, res) {
-  burgers.all(function (result) {
-    const allBurgers = {
-      burgers: result,
-    };
-    res.render("index", allBurgers);
+    burger.selectAll(function (data) {
+      var burgerObject = {
+        burgers: data,
+      };
+      res.render("index", burgerObject);
+    });
   });
-});
-
-router.post();
-
-router.put();
-
-router.delete();
-
-module.exports = router;
+  
+  router.post("/api/burgers", function (req, res) {
+    burger.firstInsert(["burger_name"], [req.body.burger_name], function (result) {
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+    burger.firstUpdate(
+      {
+        devoured: req.body.devoured,
+      },
+      condition,
+      function (result) {
+        if (result.changedRows == 0) {
+          return res.status(404).end();
+        } else {
+          res.status(200).end();
+        }
+      }
+    );
+  });
+  
+  router.delete("/api/burgers/all", function (req, res) {
+    burger.deleteAll(function (result) {
+      if (result.affectedRows == 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
+  module.exports = router;
