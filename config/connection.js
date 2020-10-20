2f0c6ca131ee25a04083cd1710cb970698e22e00
Kendall Masterson <masterson.kendall@gmail.com>
2020-10-20 19:33:25 -0400

    added .env file

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,24 @@
 var mysql = require("mysql");
 
+var connection;
 
-
-  var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "@Gail123",
-    database: "burger_db"
-  }); 
+    password: process.env.DB_PASSWORD,
+    database: "burger_db",
+  });
+}
 
 
-// Make connection.
+
+
+
+// Make connection
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -20,5 +27,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
+// Export connection for our ORM to use
 module.exports = connection;
